cfe55a5416aae9b106c8631efac1c7970b67140b
Ceren <cerenbulbul27@gmail.com>
2020-08-22 09:13:34 -0700

    Creation My Design Switch1

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -8,13 +8,12 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 
