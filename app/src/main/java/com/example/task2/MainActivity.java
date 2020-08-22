2c49fc4668671aec0cc5fdbc288eadbe48127093
Ceren <cerenbulbul27@gmail.com>
2020-08-22 09:41:32 -0700

    Creation My Design Switch3

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.task2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
 
     }
