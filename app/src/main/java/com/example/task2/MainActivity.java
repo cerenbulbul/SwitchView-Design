9d028f6675ac4fb7920a2ae39f774b4c268030c8
Ceren <cerenbulbul27@gmail.com>
2020-08-22 09:26:35 -0700

    Creation My Design Switch2

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
